f83faf7c75b1e852cf4a09b64841abaf09709353
Flogense <96473557+Flogense@users.noreply.github.com>
2021-12-22 13:19:46 +0200

    response removed

--- React App Backend/server.js
+++ React App Backend/server.js
@@ -23,20 +23,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    db.select('*').from('users')
-        .then(users => {
-            res.json(users);
-        })
-})
-
+app.get('/', (req, res) => { res.send('it is working!') })
 app.post('/signin', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(procces.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`App is running on port ${process.env.PORT}`)
 })
 
